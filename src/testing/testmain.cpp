e9d4638d6fd03f46c91378d23cb5fdb530889fe1
Jameson Thatcher <sireel@bluescreenofdoom.com>
2013-09-15 21:54:13 +0100

    [feature/shader-encapsulation] WIP Shader tests written.
    Tests cover ShaderLoader and Shader, which is a separation to mimic the
    other parts of the graphics system - it seems like a clean divide to
    make.

--- src/testing/testmain.cpp
+++ src/testing/testmain.cpp
@@ -3,13 +3,15 @@
 #include "../graphics/iimageloader.h"
 #include "../graphics/devilwrapper.h"
 #include "../graphics/textureloader.h"
+#include "../graphics/ishader.h"
+#include "../graphics/vert.h"
 
 BEGIN_TEST_DEF(DevILWrapperTest) {
 	//IImageLoader& obj = DevILWrapper("test.jpg");
 	DevILWrapper obj("test.jpg");
 	ASSERT_NEQ(0u, obj.getWidth());
 	ASSERT_NEQ(0u, obj.getHeight());
-	ASSERT_NEQ((void*)nullptr, obj.getDataPointer());
+	ASSERT_NEQ(static_cast<void*>(nullptr), obj.getDataPointer());
 	return true;
 }
 END_TEST_DEF(DevILWrapperTest);
@@ -41,6 +43,63 @@ BEGIN_TEST_DEF(TextureLoaderTest) {
 	return true;
 }
 END_TEST_DEF(TextureLoaderTest);
+
+BEGIN_TEST_DEF(ShaderTest) {
+	/*
+	 * This is the first test I've attempted to write before the code it
+	 * will test has been written. Forgive me :)
+	 */
+	std::vector<IShader*> v;
+	v.push_back(ShaderLoader::GetBasicShader());
+	std::string frag(
+R"(#version 150
+in vec4 cfv;
+in vec2 tfv;
+out vec4 oc;
+uniform sampler2D ts;
+void main() {
+	oc = cfs*texture(ts,tfv);
+})"
+	);
+	std::string vert(
+R"(#version 150
+in vec4 position;
+in vec4 colour;
+out vec4 cfv;
+in vec2 texture;
+out vec2 tfv;
+void main() {
+	gl_Position = position;
+	cfv = colour;
+	tfv = texture;
+})"
+	);
+	v.push_back(ShaderLoader::LoadShaderFromBuffers(frag,vert));
+
+	for(auto s: v) {
+		ASSERT_NEQ(nullptr, s);
+		ASSERT_GE (0, s->getShaderID());
+		for(auto a: {
+				VertComponent::Position,
+				VertComponent::Colour,
+				VertComponent::Texture
+			}
+		) {
+			ASSERT_NEQ(false, s->IsAttributeSupported(a));
+		}
+		ASSERT_GE (0, s->getNumTextureSlots());
+		for(int i = 0; i < AS_INDEX(TextureType::Count)) {
+			if(s->isTextureTypeSupported(
+				static_cast<TextureType>(i)
+			)) {
+				ASSERT_GE(0, s->getSamplerSlot(
+					static_cast<TextureType>(i)
+				));
+			}
+		}
+	}
+}
+END_TEST_DEF(ShaderTest);
 
 namespace UnitTesting {
 	bool RunTests() {

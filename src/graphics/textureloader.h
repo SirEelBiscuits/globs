b44c818718bcf0d0ccedb62c222266aaa3c7b06a
Jameson Thatcher <sireel@bluescreenofdoom.com>
2013-09-26 14:35:23 +0100

    [fix/coding-conventions] documented textureloader.h

--- src/graphics/textureloader.h
+++ src/graphics/textureloader.h
@@ -3,11 +3,20 @@
 #include "texture.h"
 #include "iimageloader.h"
 
-/*
- * Factory for textures. Currently only returns OpenGL textures. This should
- * be abstracted later if there are more options.
+/**
+ * Factory for textures.
+ *
+ * Currently only returns OpenGL textures. This should be abstracted later
+ * when there are more options.
  */
 class TextureLoader {
 public:
+	/**
+	 * Turn the image an IImageLoader has loaded into a GL texture
+	 *
+	 * \param il an image loader, that has already loaded an image.
+	 * \param type the type of texture this is to be used as.
+	 * \returns nullptr if the image load failed
+	 */
 	static Texture* LoadTexture(IImageLoader&& il, TextureType type);
 };

34a9d49331872f7e64746ec37561169891d6335f
Jameson Thatcher <sireel@bluescreenofdoom.com>
2013-09-26 14:26:26 +0100

    [fix/coding-conventions] Documented shaderloader.h

--- src/graphics/shaderloader.h
+++ src/graphics/shaderloader.h
@@ -8,12 +8,35 @@
 
 #include <string>
 
+/**
+ * Factory class for shader objects.
+ *
+ * This currently only returns ShaderGL objects.
+ */
 class ShaderLoader {
 public:
+	/**
+	 * Opens a file, reads it in and passes it to the other overload
+	 *
+	 * Don't repeat yourself :)
+	 */
 	static IShader* LoadShaderFromFiles(
 		char const* fragmentShaderFilename,
 		char const* vertexShaderFilename
 	);
+
+	/**
+	 * Creates a shader program from vertex and fragment shaders.
+	 *
+	 * This will automatically detect the supported vertex attributes
+	 * and supported texture types from the shader's arguments. To see
+	 * which names are picked up for which variables, see the appropriate
+	 * documentation.
+	 *
+	 * \returns nullptr on failure
+	 * \sa VertComponent
+	 * \sa TextureType
+	 */
 	static IShader* LoadShaderFromBuffers(
 		std::string const& fragmentShader,
 	       	std::string const& vertexShader
